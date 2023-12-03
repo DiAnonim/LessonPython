e2c1f97ecfe29b8f0e44ae5690b26d392d72e5fa
DiAnonim <akopyan.2002@mail.ru>
2023-12-03 22:58:06 +0600

    Первый урок

--- LessonPython/LessonPython.py
+++ LessonPython/LessonPython.py
@@ -1,1 +1,39 @@
+# floats = [1.2, 3.5, 6.4]
 
+# a, b, c = floats
+
+# floats_divided = [x / 2 for x in floats]
+
+# for f in floats:
+#   print(f)
+
+# floats.append("me")
+
+# floats.insert(2, "you")
+
+# floats.extend([True, False])
+
+# print(floats)
+
+# floats.pop(1);
+
+# floats.remove(True);
+
+# print(floats)
+
+# i = 0
+# while i < len(floats):
+#    print(floats[i])
+#    i += 1
+
+
+# st = "lkdfjgdllgmd"
+# print(st[:5:])
+# print(st[-3::])
+# print(st[::2])
+# print(st[1::2])
+
+# a = [[1, 2, 3], "alikhan", "red", "car", [["astana", "almaty"]]]
+# print(a[0][1])
+# print(a[2])
+# print(a[4][0][1])
