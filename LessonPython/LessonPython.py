a79939ad2e3807ef5b6d59a228e0572b16536651
DiAnonim <akopyan.2002@mail.ru>
2023-12-06 21:30:17 +0600

    Lesson 4

--- LessonPython/LessonPython.py
+++ LessonPython/LessonPython.py
@@ -1,1 +1,20 @@
 
+# 2
+
+from ast import Mult
+
+
+inpA = 9
+inpB = 5
+
+
+def MultipTable(a, b):
+    for i in range(0, a + 1):
+        for j in range(0, i):
+            if j <= i:
+                print("*", end=" ")
+       
+        print()
+
+
+MultipTable(int(inpA), int(inpB))
