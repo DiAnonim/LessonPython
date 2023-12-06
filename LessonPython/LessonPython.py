3aa7c209f8c8b6d16c56da9bc0333f2b3fed25e2
DiAnonim <akopyan.2002@mail.ru>
2023-12-06 21:30:40 +0600

    Lesson 4

--- LessonPython/LessonPython.py
+++ LessonPython/LessonPython.py
@@ -1,20 +1,66 @@
 
-# 2
+
+# 1
+inpA = input()
+inpB = input()
+
+if int(inpA) % int(inpB) == 0:
+    print("divisible")
+else:
+    print("not divisible")
+
+
+
+#2
+
+inpYear = input();
+
+def LeapYear(year):
+    if int(year)%4==0 and int(year) % 100 != 0 or int(year)%400==0: print("Leap")
+    else: print("Not Leap")
+
+LeapYear(inpYear);
+
+
+
+#3
+
+inpN= input();
+
+def cntProgram(cnt):
+    if int(cnt) == 1: print(f"{cnt} Programmist")
+    elif 1 < int(cnt) < 5 : print(f"{cnt} Programmista")
+    elif 4 < int(cnt) < 100 : print(f"{cnt} Programmistov")
+    else: print("There are no programmers")
+
+cntProgram(inpN);
+
+
+
+#4
+i = 0;
+while i < 21:
+    n = 2**i
+    print(n)
+    i += 1
+
+
+#5
 
 from ast import Mult
 
 
-inpA = 9
-inpB = 5
+inpA = input()
+inpB = input()
+inpC = input()
+inpD = input()
 
 
-def MultipTable(a, b):
-    for i in range(0, a + 1):
-        for j in range(0, i):
-            if j <= i:
-                print("*", end=" ")
-       
-        print()
+def MultipTable(a, b, c, d):
+    for i in range(a, b+1):
+        print(f"{i}")
+        for j in range(c, d+1):
+            res = j*i
+            print(f"{j} {res}")
 
-
-MultipTable(int(inpA), int(inpB))
+MultipTable(int(inpA), int(inpB), int(inpC), int(inpD),)
