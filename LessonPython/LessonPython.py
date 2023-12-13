c2cf2dd81e6c5a245ed41d16b6535a7ba08b2743
DiAnonim <akopyan.2002@mail.ru>
2023-12-13 21:51:02 +0600

    итераторы и декораторы

--- LessonPython/LessonPython.py
+++ LessonPython/LessonPython.py
@@ -1,1 +1,116 @@
 
+#Создать генератор списка из исходного, который:
+#1.берет только четные значения, отрицательные возводит в куб, остальные в квадрат
+
+#def new_generator(lst):
+#    for x in lst:
+#        if x % 2 == 0:
+#            yield x**3 if x < 0 else x**2
+
+#lst = [1, 2, 3, 4, 5, -6, -7, 8, 9]
+#res_generator = new_generator(lst)
+
+#print("Исходный список:", lst)
+#print("Преобразованный генератор:")
+#for value in res_generator:
+#    print(value)
+
+
+
+#2. считает длину строк для списка из строк
+
+#lst = ["aaa", "bb", "dddd", "ggggg"]
+#generator = (len(s) for s in lst)
+
+#print("Исходный список строк:", lst)
+#print("Длины строк в генераторе:")
+#for length in generator:
+#    print(length)
+
+
+#3. список квадратов четных чисел
+
+#def new_generator(lst):
+#    for x in lst:
+#        if x % 2 == 0:
+#            yield x**2
+
+#lst = [1, 2, 3, 4, 5, -6, -7, 8, 9]
+#generator = new_generator(lst)
+
+#print("Исходный список строк:", lst)
+#print("Длины строк в генераторе:")
+#for i in generator:
+#    print(i)
+
+#4. только положительные, кратные 5, отрицательные заменить на 0
+#def new_generator(lst):
+#    for x in lst:
+#        if x > 0 and x % 5 == 0:
+#            yield x
+#        else: yield 0
+
+#lst = [10, 2, 3, 15, 5, -6, -7, 8, 9]
+#generator = new_generator(lst)
+
+#print("Исходный список строк:", lst)
+#print("Длины строк в генераторе:")
+#for i in generator:
+#    print(i)
+
+#5. из строки – только гласные буквы.
+
+#vowels = "aeiouAEIOU"
+#lst = ["mama", "banana", "chair", "milk"]
+#generator = (char for s in lst for char in s if char in vowels)
+
+#print("исходный список строк:", lst)
+#print("длины строк в генераторе:")
+#for length in generator:
+#    print(length)
+
+
+##Написать декоратор, который оборачивает строку в теги <i></i>.
+
+#def tagI(f1):
+#    def tagPrintI():
+#        print("<i>", end="")
+#        f1()
+#        print("<i>", end="")
+#    return tagPrintI
+
+##Написать декоратор, который оборачивает строку в теги <strong></strong>
+
+#def tagStrong(f1):
+#    def tagPrintStrong():
+#        print("<strong>", end="")
+#        f1()
+#        print("<strong>", end="")
+#    return tagPrintStrong
+
+
+##Применить оба декоратора
+#@tagStrong
+#@tagI
+#def stringPrint():
+#    print("aaaaaaaaaaaaaaaa", end="")
+
+#stringPrint()
+
+#Написать декоратор, который в любую функцию, в __doc__ дописывает имя и фамилию
+#автора.
+
+def doc(f1):
+    def docPrint():
+        f1.__doc__ += f"\n\nAuthor: author_name author_surname"
+        
+    return docPrint
+
+@doc
+def stringPrint():
+    """Print String"""
+    print("aaaaaaaaaaaaaaaa", end="")
+
+
+stringPrint()
+print(stringPrint.__doc__)
